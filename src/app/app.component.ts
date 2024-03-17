05581a591e73db9c0fbe6018119284d395a8ec30
dc1248 <dorothywccheng@gmail.com>
2024-03-17 02:26:57 +0100

    added routing and changed main app component to using routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout'
 import { RouterOutlet } from '@angular/router';
+
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon'
+
+import { PokemonComponent } from './component/pokemon/pokemon.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavModule, MatIconModule, PokemonComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'pokegeek';
+
+
+  @ViewChild(MatSidenav) //allows access to sidenav bar
+  sidenav!: MatSidenav; //sidenav will not be nullified
+ 
+  constructor(private observer: BreakpointObserver) {} //receives dependencies
+ 
+  ngAfterViewInit() {
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PokemonDetailComponent } from './component/pokemon-detail/pokemon-detail.component';
+import { PokemonComponent } from './component/pokemon/pokemon.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo: '/pokemon', pathMatch: 'full'},
+    {path:'pokemon', component: PokemonComponent},
+    {path:'pokemon-detail/:pokemonName', component: PokemonDetailComponent}
+];
