ce879c3ea3ca80e2c9419b20a5b14c7e827cc37a
dc1248 <dorothywccheng@gmail.com>
2024-03-20 17:05:02 +0100

    changed backend source in production environment file

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     pokemonURL: 'https://pokeapi.co/api/v2/pokemon',
     pokemonLimit: '10000',
-    backendURL: 'http://localhost:8080/api'
+    backendURL: 'http://157.230.114.115:8080/api'
 };
