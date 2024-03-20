aca662c70aa40d6203c1f2a21862a6c2a20e510b
dc1248 <dorothywccheng@gmail.com>
2024-03-20 01:56:41 +0100

    modified pokemon detail component to display current rating summary and allow user rating

--- frontend/src/app/component/pokemon-detail/pokemon-detail.component.ts
+++ frontend/src/app/component/pokemon-detail/pokemon-detail.component.ts
@@ -7,29 +7,49 @@ import { PokemonDetail } from '../../model/pokemon-detail';
 import { MatCardModule } from '@angular/material/card';
 import { TypeBackgroundColor } from '../../model/type-background-color';
 import { TypeColor } from '../../model/type-color';
-import { CommonModule } from '@angular/common';
+import { CommonModule} from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
+import { RatingService } from '../../service/rating.service';
+import { PokemonRatingSummaryApiResponse } from '../../model/pokemon-rating-summary-api-response';
+import { NgxStarsModule } from 'ngx-stars';
+import { PokemonRatingDto } from '../../model/pokemon-rating-dto';
 
 @Component({
   selector: 'app-pokemon-detail',
   standalone: true,
-  imports: [MatCardModule, CommonModule, HeaderComponent],
+  imports: [MatCardModule, CommonModule, NgxStarsModule, HeaderComponent],
   templateUrl: './pokemon-detail.component.html',
   styleUrl: './pokemon-detail.component.scss'
 })
 export class PokemonDetailComponent implements OnInit{
 
   pokemon?: Pokemon;
+  userRating?: number;
 
-  constructor(private pokemonService: PokemonService, private route: ActivatedRoute){}
+  constructor(private pokemonService: PokemonService, private ratingService: RatingService, private route: ActivatedRoute){}
 
   ngOnInit(): void {
 
     this.route.params.subscribe(params => this.pokemon = {name: params['pokemonName'], url: `${environment.pokemonURL}/${params['pokemonName']}`});
     if (this.pokemon) {
+      this.getRatingSummary(this.pokemon);
       this.getPokemonDetails(this.pokemon);
     }
 
+  }
+
+  getRatingSummary(pokemon: Pokemon): void {
+    this.ratingService.getRatingSummary(pokemon).subscribe((pokemonRatingSummaryApiResponse: PokemonRatingSummaryApiResponse) => {
+      if (pokemonRatingSummaryApiResponse) {
+        pokemon.rating = pokemonRatingSummaryApiResponse.averageRatings;
+        pokemon.rating_number = pokemonRatingSummaryApiResponse.totalRatings;
+      }
+      else {
+        pokemon.rating = 0;
+        pokemon.rating_number = 0;
+      }
+      console.log(pokemon.rating);
+    })
   }
 
   getPokemonDetails(pokemon: Pokemon): void {
@@ -45,4 +65,26 @@ export class PokemonDetailComponent implements OnInit{
   getTypeBackgroundColor(type: string): string {
     return (TypeBackgroundColor as Record<string, string>)[type];
   }
+
+  onRatingSet(userRating: number): void {
+    if (this.pokemon && window.confirm(`Do you want to give a rating of ${userRating} to this pokemon?`)) {
+      this.userRating = userRating;
+      let pokmeonRating: PokemonRatingDto = {
+        pokemonName: this.pokemon.name,
+        rating: this.userRating
+      };
+      this.ratingService.saveRating(pokmeonRating).subscribe((pokemonRatingSummaryApiResponse: PokemonRatingSummaryApiResponse) => {
+        if (this.pokemon) {
+          this.pokemon.rating = pokemonRatingSummaryApiResponse.averageRatings;
+          this.pokemon.rating_number = pokemonRatingSummaryApiResponse.totalRatings;
+        }
+      });
+      alert(`Thank you for rating! This window will now be reloaded.`);
+      window.location.reload();
+    }
+    else {
+      alert(`The rating has been cancelled.`)
+    }
+  }
+
 }
